2a0a342c4dfee8e4075d49ae3ddc41693f8e9f9b
tripathi-aditya <adityaomail@gmail.com>
2023-11-07 19:54:49 +0530

    refactor to check all containers

--- example.tsx
+++ example.tsx
@@ -1,18 +1,38 @@
 import React from "react";
-import { toaster, ToastContainer } from "./src";
+import { toaster, ToastContainers, POSITIONS } from "./src";
 
 const Example = () => {
-  const handleToast = () =>
+  const handleToast = () => {
     toaster({
       variant: "success",
       message: "Hello World!",
       onLoadCB: () => console.log("Toast Loaded"),
+      position: POSITIONS.TOP,
     });
+    toaster({
+      variant: "success",
+      message: "Hello World!",
+      onLoadCB: () => console.log("Toast Loaded"),
+      position: POSITIONS.RIGHT,
+    });
+    toaster({
+      variant: "success",
+      message: "Hello World!",
+      onLoadCB: () => console.log("Toast Loaded"),
+      position: POSITIONS.BOTTOM,
+    });
+    toaster({
+      variant: "success",
+      message: "Hello World!",
+      onLoadCB: () => console.log("Toast Loaded"),
+      position: POSITIONS.LEFT,
+    });
+  };
 
   return (
     <div>
       <button onClick={handleToast}>Toast Me Up !</button>
-      <ToastContainer />
+      <ToastContainers />
     </div>
   );
 };
