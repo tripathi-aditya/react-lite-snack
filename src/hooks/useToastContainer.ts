7518c155d2c62f79f14afef5d4ed6953d1028788
tripathi-aditya <adityaomail@gmail.com>
2023-11-09 22:01:03 +0530

    update subscribe hook

--- src/hooks/useToastContainer.ts
+++ src/hooks/useToastContainer.ts
@@ -1,19 +1,62 @@
-import { useEffect, useState } from "react";
-import { useStore } from "../core/store";
-import { StoreState, ToastEntity } from "../types";
+import { useEffect, useReducer, useState } from "react";
+import { useStoreEffect } from "../core/store";
+import {
+  POSITIONS,
+  StoreActions,
+  StoreState,
+  TOAST_OPERATIONS,
+  ToastEntity,
+} from "../types";
+
+const initialContainerBuckets = new Map([
+  [POSITIONS.TOP, [] as ToastEntity[]],
+  [POSITIONS.RIGHT, [] as ToastEntity[]],
+  [POSITIONS.BOTTOM, [] as ToastEntity[]],
+  [POSITIONS.LEFT, [] as ToastEntity[]],
+]);
 
 export const useToastContainer = () => {
-  const [toasts, setToasts] = useState<ToastEntity[]>([]);
-  const sbscribeToStore = (storeState: StoreState) => {
-    let { toasts } = storeState;
-    setToasts([...toasts]);
+  function containersReducer(state: Containers, action: ContainerActions) {
+    const { data: toastEntity, type } = action;
+    switch (type) {
+      case TOAST_OPERATIONS.UPSERT:
+        return new Map(state).set(toastEntity?.position, [
+          ...(state.get(toastEntity?.position) || []),
+          toastEntity,
+        ]);
+      case TOAST_OPERATIONS.DELETE:
+        return new Map(state).set(
+          toastEntity?.position,
+          state
+            .get(toastEntity?.position)
+            ?.filter((toast) => toast.id !== toastEntity.id) || []
+        );
+      default:
+        return state;
+    }
+  }
+  const [toastContainers, dispatch] = useReducer(
+    containersReducer,
+    initialContainerBuckets
+  );
+
+  const onStoreSubscribe = (
+    storeState: StoreState,
+    operationData: StoreActions
+  ) => {
+    // because we have no custom local types as of now
+    dispatch({ ...operationData });
   };
+
   useEffect(() => {
-    useStore(sbscribeToStore);
-  }, [toasts]);
+    useStoreEffect(onStoreSubscribe);
+    return () => useStoreEffect(onStoreSubscribe, true);
+  }, []);
 
   return {
-    toasts,
-    setToasts,
+    toastContainers,
   };
 };
+
+type Containers = Map<POSITIONS, ToastEntity[]>;
+type ContainerActions = StoreActions;
