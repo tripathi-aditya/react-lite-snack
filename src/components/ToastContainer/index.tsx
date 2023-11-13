9f6cbeeff2229f2547438426c7bc2d4c4b312aba
tripathi-aditya <adityaomail@gmail.com>
2023-11-13 10:34:13 +0530

    test produce in all containers

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,14 +1,57 @@
+import "./toastContainer.css";
 import { Toast } from "..";
 import { useToastContainer } from "../../hooks/useToastContainer";
+import { POSITIONS } from "../..";
 
-export const ToastContainer = () => {
-  const { toasts } = useToastContainer();
+export const ToastContainers = () => {
+  const { toastContainers } = useToastContainer();
+
+  const ToastContainerTop = () => {
+    return (
+      <div className="container container__top">
+        {toastContainers.get(POSITIONS.TOP)?.map((toast) => (
+          <Toast key={toast.id} {...toast} />
+        ))}
+      </div>
+    );
+  };
+
+  const ToastContainerRight = () => {
+    return (
+      <div className="container container__right">
+        {toastContainers.get(POSITIONS.RIGHT)?.map((toast) => (
+          <Toast key={toast.id} {...toast} />
+        ))}
+      </div>
+    );
+  };
+
+  const ToastContainerBottom = () => {
+    return (
+      <div className="container container__bottom">
+        {toastContainers.get(POSITIONS.BOTTOM)?.map((toast) => (
+          <Toast key={toast.id} {...toast} />
+        ))}
+      </div>
+    );
+  };
+
+  const ToastContainerLeft = () => {
+    return (
+      <div className="container container__left">
+        {toastContainers.get(POSITIONS.LEFT)?.map((toast) => (
+          <Toast key={toast.id} {...toast} />
+        ))}
+      </div>
+    );
+  };
 
   return (
-    <div>
-      {toasts?.map((toast) => (
-        <Toast key={Math.random()} {...toast} />
-      ))}
-    </div>
+    <>
+      <ToastContainerTop />
+      <ToastContainerRight />
+      <ToastContainerBottom />
+      <ToastContainerLeft />
+    </>
   );
 };
