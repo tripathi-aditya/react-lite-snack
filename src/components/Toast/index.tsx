bda0490f1d51bea0b81cd55cf674b68049a520ca
tripathi-aditya <adityaomail@gmail.com>
2023-11-17 22:19:42 +0530

    toast syling and container fixes

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,10 +1,18 @@
 import { useEffect } from "react";
 import { ToastProps } from "../../types";
+import "./toast.css";
+import { variantToClass } from "../../utils";
 
 export const Toast = ({ variant, message, onLoadCB }: ToastProps) => {
   useEffect(() => {
     onLoadCB && onLoadCB();
   }, []);
 
-  return <div>{message}</div>;
+  return (
+    <div className={`toast toast_variant__${variant}`}>
+      <section className="toast_icon">{/* icon */}</section>
+      <span className="toast_message">{message}</span>
+      <span className="toast_close">{/* close icon */}</span>
+    </div>
+  );
 };
