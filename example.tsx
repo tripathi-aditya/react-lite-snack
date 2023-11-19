0c25aee4de8529afcc12ee5dbd31c28c8f7daccc
tripathi-aditya <adityaomail@gmail.com>
2023-11-19 11:02:41 +0530

    test onClose callback

--- example.tsx
+++ example.tsx
@@ -6,25 +6,31 @@ const Example = () => {
     toaster({
       variant: "success",
       message: "Hello World!",
-      onLoadCB: () => console.log("Toast Loaded"),
+      onLoadCB: () => console.log("Toast Loaded 1"),
+      onCloseCB: () => console.log("Toast Clsoed 1"),
       position: POSITIONS.TOP,
     });
     toaster({
-      variant: "success",
       message: "Hello World!",
-      onLoadCB: () => console.log("Toast Loaded"),
+      onLoadCB: () => console.log("Toast Loaded 2"),
+      onCloseCB: () => console.log("Toast Clsoed 2"),
+
       position: POSITIONS.RIGHT,
     });
     toaster({
-      variant: "success",
+      variant: "info",
       message: "Hello World!",
-      onLoadCB: () => console.log("Toast Loaded"),
+      onLoadCB: () => console.log("Toast Loaded 3"),
+      onCloseCB: () => console.log("Toast Clsoed 3"),
+
       position: POSITIONS.BOTTOM,
     });
     toaster({
-      variant: "success",
+      variant: "warning",
       message: "Hello World!",
-      onLoadCB: () => console.log("Toast Loaded"),
+      onLoadCB: () => console.log("Toast Loaded 4"),
+      onCloseCB: () => console.log("Toast Clsoed 4"),
+
       position: POSITIONS.LEFT,
     });
   };
