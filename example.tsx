d78d07c23af2912d52eba81ad242ae85f1f07b67
tripathi-aditya <adityaomail@gmail.com>
2023-12-13 10:21:20 +0530

    variants from string to enum

--- example.tsx
+++ example.tsx
@@ -1,36 +1,31 @@
 import React from "react";
-import { toaster, ToastContainers, POSITIONS } from "./src";
+import { toaster, ToastContainers, POSITIONS, VARIANTS } from "./src";
 
 const Example = () => {
   const handleToast = () => {
     toaster({
-      variant: "success",
+      variant: VARIANTS.SUCCESS,
       message: "Hello World!",
       onLoadCB: () => console.log("Toast Loaded 1"),
       onCloseCB: () => console.log("Toast Clsoed 1"),
-      position: POSITIONS.TOP,
     });
     toaster({
       message: "Hello World!",
       onLoadCB: () => console.log("Toast Loaded 2"),
       onCloseCB: () => console.log("Toast Clsoed 2"),
-
-      position: POSITIONS.RIGHT,
     });
     toaster({
-      variant: "info",
+      variant: VARIANTS.INFO,
       message: "Hello World!",
       onLoadCB: () => console.log("Toast Loaded 3"),
       onCloseCB: () => console.log("Toast Clsoed 3"),
-
       position: POSITIONS.BOTTOM,
     });
     toaster({
-      variant: "warning",
+      variant: VARIANTS.WARNING,
       message: "Hello World!",
       onLoadCB: () => console.log("Toast Loaded 4"),
       onCloseCB: () => console.log("Toast Clsoed 4"),
-
       position: POSITIONS.LEFT,
     });
   };
